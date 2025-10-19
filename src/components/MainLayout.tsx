75f99690324ba572b436a846979c407a6beebf99
vouudaonhan <bi0343324552bo@gmail.com>
2025-10-19 09:29:53 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Navigation } from "./Navigation";
 import { Dashboard } from "./Dashboard";
-import { Notifications } from "./Notifications";
+import Notifications from "./Notifications";
 import { AccountManagement } from "./AccountManagement";
 import { 
   FileText, 
